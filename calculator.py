97186d1e170f2b67edf5cdd9a19969be9c35167f
rajuf6 <rajupanugantif6@gmail.com>
2020-06-04 08:41:04 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,18 @@
+num1 = float(input('Enter first num: '))
+op = input('enter operator: ')
+num2 = float(input('Enter second  num: '))
+
+if op =='+':
+    print(num1+num2)
+
+elif op =='-':
+    print(num1-num2)
+
+elif op =='/':
+    print(num1/num2)    
+
+elif op =='*':
+    print(num1*num2)    
+
+else:
+    print('invlaid operator ')    
